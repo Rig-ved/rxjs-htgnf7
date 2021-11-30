55fa0a928abe97996a84c4b889ebe998c8e22913
Rig-ved <rick.bhattacharya@gmail.com>
2021-11-30 14:49:03 +0530

    1 file modified, 1 added

---
+++ advanced/ReplaySubject.ts
@@ -0,0 +1,12 @@
+import { ReplaySubject } from 'rxjs';
+
+const sub = new ReplaySubject(2);
+
+// Replay subject will replay all the values
+
+sub.next(1);
+sub.next(2);
+sub.next(3);
+sub.next(4);
+sub.subscribe(console.log);
+sub.next(5);

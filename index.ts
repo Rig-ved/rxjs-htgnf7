47e9adb4a8abb7ec0b627df31a1b8cb250ffc7aa
Rig-ved <rick.bhattacharya@gmail.com>
2021-11-30 22:11:01 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,59 +1,23 @@
-import {
-  combineLatest,
-  fromEvent,
-  interval,
-  Observable,
-  pluck,
-  tap,
-} from 'rxjs';
-console.clear();
-import {
-  combineLatestWith,
-  delay,
-  distinctUntilChanged,
-  filter,
-  map,
-  mapTo,
-  share,
-  switchMap,
-  take,
-  withLatestFrom,
-} from 'rxjs/operators';
-import './style.css';
+import { Observable, asyncScheduler } from 'rxjs';
+import { observeOn } from 'rxjs/operators';
 
-const qty: Element = document.querySelector('#qty');
-const rate = document.querySelector('#rate');
+const observable = new Observable((observer) => {
+  observer.next(1);
+  observer.next(2);
+  observer.next(3);
+  observer.complete();
+}).pipe(observeOn(asyncScheduler));
 
-const display = document.querySelector('#displayContent');
-
-const qty$ = interval(3000).pipe(
-  map((data) => data + 1),
-  take(10)
-);
-
-qty$.subscribe((data) => {
-  qty.setAttribute('value', String(data));
+console.log('just before subscribe');
+observable.subscribe({
+  next(x) {
+    console.log('got value ' + x);
+  },
+  error(err) {
+    console.error('something wrong occurred: ' + err);
+  },
+  complete() {
+    console.log('done');
+  },
 });
-const rate$: Observable<number> = fromEvent(rate, 'input').pipe(
-  pluck('target', 'value'),
-  map((item) => +item)
-);
-
-const calculate = (qty, rate) => {
-  if (qty == 0 || rate == 0) {
-    return String(10);
-  }
-
-  return String(+qty * +rate);
-};
-
-const combine$ = rate$.pipe(
-  withLatestFrom(qty$),
-  map(([rate, qty]) => {
-    return calculate(qty, rate);
-  })
-);
-
-combine$.subscribe((data) => {
-  display.innerHTML = data;
-});
+console.log('just after subscribe');

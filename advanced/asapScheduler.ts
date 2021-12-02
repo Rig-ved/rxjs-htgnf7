f1fd46bbe0c59e26b70d2e8321194051a925084c
Rig-ved <rick.bhattacharya@gmail.com>
2021-12-02 19:37:41 +0530

    1 file modified, 1 added

---
+++ advanced/asapScheduler.ts
@@ -0,0 +1,35 @@
+import { asapScheduler, asyncScheduler, range } from 'rxjs';
+
+const observer = {
+  next: (val) => console.log('hello : ', val),
+  error: (err) => console.log('Error ', err),
+  complete: () => {
+    console.log('Completed ');
+  },
+};
+
+// Asap Schedulers are like queueMicrotask  which will immediately invoke after all the synchronous operations have finished but before waiting for the queue to throw other async tasks
+
+// to prove this lets understand the functionality of the new programming of tasks which doesnt wait
+
+const obs$ = range(1, 100).subscribe((data) => {
+  console.log(data);
+});
+
+Promise.resolve('Promise function').then(console.log);
+
+queueMicrotask(() => {
+  console.log('From microtask');
+});
+
+// Here you can see async scheduler gets called at last
+asyncScheduler.schedule(() => {
+  console.log('From Async Scheduler');
+});
+
+// Asap Scheduler are more like promise.resolve and queueMicrotask
+// ASAP Scheduler will block the UI
+asapScheduler.schedule(() => {
+  console.log('From Asap Scheduler');
+});
+console.log('Done GoodBye Sync tasks ');

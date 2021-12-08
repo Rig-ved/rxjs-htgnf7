20d7bd47e9e1a31444c5ef2ad110fd79accbe117
Rig-ved <rick.bhattacharya@gmail.com>
2021-12-08 18:27:33 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,15 +1,1 @@
-import { interval, of, take } from 'rxjs';
-import { takeLast, takeUntil, takeWhile } from 'rxjs/operators';
-
-const sub = of(
-  'Fred',
-  'Hello',
-  'Rick',
-  'Worl',
-  'a',
-  'rgdhs',
-  'hgth',
-  'Scott'
-).pipe(takeLast(1));
-
-sub.subscribe(console.log);
+console.log('Hello');
